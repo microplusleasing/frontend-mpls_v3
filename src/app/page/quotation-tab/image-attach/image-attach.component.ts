d3ec2471c89e866d77bef13e97a3dd55fe5285cf
Napat Rodniem <napatlenovol13@outlook.co.th>
2023-09-26 16:57:41 +0700

    update admin prevent upload image

--- src/app/page/quotation-tab/image-attach/image-attach.component.ts
+++ src/app/page/quotation-tab/image-attach/image-attach.component.ts
@@ -21,6 +21,7 @@ import { IResImageTypeAttachMultipleData } from 'src/app/interface/i-res-image-t
 import { IResImageAttachMultipleData } from 'src/app/interface/i-res-image-attach-multiple';
 import { HttpErrorResponse } from '@angular/common/http';
 import { IImageAttachUploadMultiple } from 'src/app/interface/i-image-attach-upload-multiple';
+import { IUserTokenData } from 'src/app/interface/i-user-token';
 
 
 @Component({
@@ -32,6 +33,8 @@ export class ImageAttachComponent extends BaseService implements OnInit {
   @Input() quotationReq = {} as Observable<IResQuotationDetail>;
   @Output() emitverifyimageattach = new EventEmitter();
   @Output() emitverifysecondhandcarimage = new EventEmitter();
+
+  userSession: IUserTokenData = {} as IUserTokenData
 
   quotationdatatemp: IResQuotationDetail = {} as IResQuotationDetail
 
@@ -110,7 +113,18 @@ export class ImageAttachComponent extends BaseService implements OnInit {
   }
 
   ngOnInit() {
+    this.getUserSessionQuotation().subscribe({
+      next: (res_session) => {
 
+        this.userSession = res_session
+      }, error: (e) => {
+
+      }, complete: () => {
+
+      }
+    }).add(() => {
+      this.loadingService.hideLoader();
+    })
   }
 
   onStageChageFormStepper() {
@@ -209,7 +223,7 @@ export class ImageAttachComponent extends BaseService implements OnInit {
                             urlsanitizer: this.sanitizer.bypassSecurityTrustUrl(imageStr),
                             src: imageStr
                           });
-                          
+
                         })
                       }
                     }

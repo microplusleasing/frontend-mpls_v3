8a7b55ecc5736f60a88bc3aca02160d911fe556a
Napat Rodniem <napatlenovol13@outlook.co.th>
2023-09-26 15:55:37 +0700

    update KYC image show

--- src/app/page/quotation-tab/image-attach/image-attach.component.ts
+++ src/app/page/quotation-tab/image-attach/image-attach.component.ts
@@ -187,14 +187,27 @@ export class ImageAttachComponent extends BaseService implements OnInit {
                       if (res1.data && res1.data.length !== 0) {
                         res1.data.forEach(async (item) => {
                           const imageStr = this._arrayBufferToJpeg(item.image_file.data)
+                          // this.uploadedImages.push({
+                          //   name: item.image_name ?? '',
+                          //   image_code: item.image_code ?? '',
+                          //   image_header: this.temp_master_categories.find((m_image) => m_image.image_code == item.image_code)?.image_header ?? '',
+                          //   image_field_name: item.image_name ?? '',
+                          //   urlsanitizer: this.sanitizer.bypassSecurityTrustUrl(imageStr),
+                          //   src: imageStr
+                          // })
                           this.uploadedImages.push({
                             name: item.image_name ?? '',
                             image_code: item.image_code ?? '',
-                            image_header: this.temp_master_categories.find((m_image) => m_image.image_code == item.image_code)?.image_header ?? '',
+                            image_header: this.temp_master_categories.find((m_image) => {
+                              if (m_image.image_code === '16') {
+                                return true; // Set image_header to 'KYC'
+                              }
+                              return m_image.image_code == item.image_code;
+                            })?.image_code === '16' ? 'KYC' : this.temp_master_categories.find((m_image) => m_image.image_code == item.image_code)?.image_header ?? '', // Set image_header to 'KYC' if image_code is '16'
                             image_field_name: item.image_name ?? '',
                             urlsanitizer: this.sanitizer.bypassSecurityTrustUrl(imageStr),
                             src: imageStr
-                          })
+                          });
                         })
                       }
                     }

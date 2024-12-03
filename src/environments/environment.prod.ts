9a0d196d4cd359bca39f6c29d57d288e1794d453
Napat Rodniem <developbyramire94@gmail.com>
2024-12-03 16:55:29 +0700

    update version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -42,7 +42,7 @@ export const environment = {
 //   iappapikey: "F3kBa0O7D07SUYy5YmcfLeohWRxbipdn",
 //   faceRecogApi: "https://api.iapp.co.th/thai-national-id-card/v3/front",
 //   faceValidApi: "https://api.iapp.co.th/face-verification/v2/face_compare",
-//   version: "version  1.05.06"
+//   version: "version  1.05.07"
 // };
 
 // ===== api v1 =====

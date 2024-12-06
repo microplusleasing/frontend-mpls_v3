d5f61ab4b054bf8884c0fdba8c99558d4f47e257
Napat Rodniem <developbyramire94@gmail.com>
2024-12-06 14:48:31 +0700

    update component (face-valid-edit handle ng 19)

--- src/app/widget/dialog/permission-upload-facecompare-dialog/permission-upload-facecompare-dialog.component.ts
+++ src/app/widget/dialog/permission-upload-facecompare-dialog/permission-upload-facecompare-dialog.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-permission-upload-facecompare-dialog',
-  templateUrl: './permission-upload-facecompare-dialog.component.html',
-  styleUrls: ['./permission-upload-facecompare-dialog.component.scss']
+    selector: 'app-permission-upload-facecompare-dialog',
+    templateUrl: './permission-upload-facecompare-dialog.component.html',
+    styleUrls: ['./permission-upload-facecompare-dialog.component.scss'],
+    standalone: false
 })
 export class PermissionUploadFacecompareDialogComponent {
 
